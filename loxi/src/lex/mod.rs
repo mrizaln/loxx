f1228fea4c39c982138ea1404e4d6eb2694a4c7f
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2024-09-07 17:42:49 +0700

    Fix code to be utf-8 compatible

--- loxi/src/lex/mod.rs
+++ loxi/src/lex/mod.rs
@@ -1,3 +1,6 @@
+use std::iter::Peekable;
+use std::str::CharIndices;
+
 use thiserror::Error;
 
 pub mod token;
@@ -19,11 +22,11 @@ pub enum LexError {
 #[derive(Debug)]
 pub struct Lexer<'a> {
     source: &'a str,
+    chars: Peekable<CharIndices<'a>>,
     lines: Vec<&'a str>,
     tokens: Vec<Token>,
     errors: Vec<LexError>,
     line: LineLocation,
-    start: usize,
 }
 
 #[derive(Debug)]
@@ -37,34 +40,31 @@ impl<'a> Lexer<'a> {
     pub fn new(program: &'a str) -> Self {
         Lexer {
             source: program,
+            chars: program.char_indices().peekable(),
             lines: Vec::new(),
             tokens: Vec::new(),
             errors: Vec::new(),
-            line: LineLocation {
-                index: 1,
-                start: 0,
-                end: 0,
-            },
-            start: 0,
+            line: LineLocation { index: 1, start: 0 },
         }
     }
 
     pub fn scan(mut self) -> ScanResult<'a> {
-        while self.start < self.source.len() {
-            self.start = self.scan_token(self.start);
-            self.line.end = self.start;
+        while let Some((i, ch)) = self.chars.next() {
+            let skip_to = self.scan_token(i, ch);
+
+            let mut diff = skip_to - i - 1;
+            while diff > 0 {
+                self.chars.next();
+                diff -= 1;
+            }
 
             // simple debugging to see whether my code is stalling, or just slow :D
-            print!(
-                "\r{} chars out of {} scanned",
-                self.start,
-                self.source.len()
-            );
+            print!("\r{} chars out of {} scanned", i, self.source.len());
         }
 
         self.tokens.push(Token {
             value: TokenValue::Eof,
-            loc: self.line.loc_rel(self.start).unwrap(),
+            loc: self.line.loc_rel(self.source.len()).unwrap(),
         });
 
         ScanResult {
@@ -74,8 +74,7 @@ impl<'a> Lexer<'a> {
         }
     }
 
-    fn scan_token(&mut self, current: usize) -> usize {
-        let single = self.source.chars().nth(current).unwrap();
+    fn scan_token(&mut self, current: usize, single: char) -> usize {
         match single {
             '\n' => self.newline_handler(current),
             '/' => self.slash_handler(current),
@@ -83,35 +82,31 @@ impl<'a> Lexer<'a> {
             c if c.is_digit(10) => self.number_handler(current),
             c if c.is_whitespace() => self.whitespace_handler(current),
             c if c.is_alphabetic() => self.alphabetic_handler(current),
-            _ => self.other_handler(current),
+            _ => self.other_handler(current, single),
         }
     }
 
     fn newline_handler(&mut self, current: usize) -> usize {
-        let LineLocation {
-            ref mut index,
-            ref mut start,
-            ref mut end,
-        } = self.line;
+        println!("start: {}, current: {}", self.line.start, current);
+        self.lines.push(&self.source[self.line.start..current]);
 
-        *end = current;
-        self.lines.push(&self.source[*start..*end]);
-
-        *index += 1;
-        *start = current + 1;
+        self.line.index += 1;
+        self.line.start = current + 1;
 
         current + 1
     }
 
     fn slash_handler(&mut self, current: usize) -> usize {
         // might be comment
-        let double_slice = &self.source[current..current + 2];
-        if double_slice == "//" {
-            let index = self.source[current..]
-                .find('\n')
-                .map(|i| i + current)
-                .unwrap_or(self.source.len());
-            return index;
+        match self.chars.peek() {
+            Some((i, '/')) => {
+                let index = self.source[*i + 1..]
+                    .find('\n')
+                    .map(|i| i + current)
+                    .unwrap_or(self.source.len());
+                return index;
+            }
+            _ => (),
         }
 
         self.tokens.push(Token {
@@ -122,30 +117,33 @@ impl<'a> Lexer<'a> {
     }
 
     fn string_handler(&mut self, current: usize) -> usize {
-        let index = self.source[current + 1..]
-            .find('"')
-            .map(|i| i + current + 1);
+        let start_line = self.line.clone();
+
+        let mut index = None;
+        for (i, ch) in self.source[current + 1..].char_indices() {
+            if ch == '"' {
+                index = Some(current + 1 + i);
+                break;
+            } else if ch == '\n' {
+                self.newline_handler(current + 1 + i);
+            }
+        }
 
         match index {
             Some(idx) => {
                 let value = self.source[current + 1..idx - 1].to_string();
                 self.tokens.push(Token {
                     value: TokenValue::Literal(tokens::Literal::String(value)),
-                    loc: self.line.loc_rel(current).unwrap(),
+                    loc: start_line.loc_rel(current).unwrap(),
                 });
                 idx + 1
             }
             None => {
                 self.errors.push(LexError::UnterminatedString(
-                    self.line.loc_rel(current).unwrap(),
+                    start_line.loc_rel(current).unwrap(),
                 ));
 
-                // guesstimate end of statement
-                let end = self.source[current..]
-                    .find(|c: char| c == '\n')
-                    .map(|i| i + current)
-                    .unwrap_or(self.source.len());
-                end
+                self.source.len()
             }
         }
     }
@@ -200,9 +198,7 @@ impl<'a> Lexer<'a> {
         index
     }
 
-    fn other_handler(&mut self, current: usize) -> usize {
-        let single = self.source.chars().nth(current).unwrap();
-
+    fn other_handler(&mut self, current: usize, single: char) -> usize {
         if let Ok(token) = tokens::Punctuation::try_from(single) {
             self.tokens.push(Token {
                 value: TokenValue::Punctuation(token),
@@ -211,18 +207,25 @@ impl<'a> Lexer<'a> {
             return current + 1;
         }
 
-        // for double cchars operators
-        let double_slice = &self.source[current..current + 2];
-        if let Ok(token) = tokens::Operator::try_from(double_slice) {
-            self.tokens.push(Token {
-                value: TokenValue::Operator(token),
-                loc: self.line.loc_rel(current).unwrap(),
-            });
-            return current + 2;
+        let mut two_char_buf = [0u8; 8];
+
+        // for double chars operators
+        match self.chars.peek() {
+            Some((_, ch)) => {
+                let double_slice = to_str(&mut two_char_buf, single, Some(*ch));
+                if let Ok(token) = tokens::Operator::try_from(double_slice) {
+                    self.tokens.push(Token {
+                        value: TokenValue::Operator(token),
+                        loc: self.line.loc_rel(current).unwrap(),
+                    });
+                    return current + 2;
+                }
+            }
+            _ => (),
         }
 
         // for single chars operators
-        let single_slice = &self.source[current..current + 1];
+        let single_slice = to_str(&mut two_char_buf, single, None);
         if let Ok(token) = tokens::Operator::try_from(single_slice) {
             self.tokens.push(Token {
                 value: TokenValue::Operator(token),
@@ -245,18 +248,16 @@ impl<'a> Lexer<'a> {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct LineLocation {
     pub index: usize,
     pub start: usize,
-    pub end: usize,
 }
 
 impl LineLocation {
     pub fn col_rel(&self, index: usize) -> Option<usize> {
         match index {
             i if i < self.start => None,
-            i if i > self.end => None,
             _ => Some(index - self.start + 1), // 1-indexed
         }
     }
@@ -270,4 +271,14 @@ impl LineLocation {
             None => None,
         }
     }
+}
+
+// inspired by: https://stackoverflow.com/a/78486719
+fn to_str(buf: &mut [u8; 8], left: char, right: Option<char>) -> &str {
+    let mut pos = 0;
+    pos += left.encode_utf8(&mut buf[pos..]).len();
+    if let Some(right) = right {
+        pos += right.encode_utf8(&mut buf[pos..]).len();
+    }
+    std::str::from_utf8(&buf[..pos]).expect("The two chars must be valid utf8")
 }
