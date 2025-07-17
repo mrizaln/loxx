187e57f3c8194c392af129c8d78ef1e4d79674fc
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2025-07-17 18:22:42 +0700

    Rework Value and Memory

--- loxii/src/memory.rs
+++ loxii/src/memory.rs
@@ -1,86 +1,56 @@
-#![allow(dead_code)]
+use thiserror::Error;
 
-pub struct Value {}
-
-#[derive(Debug, Copy, Clone)]
-pub struct HeapId {
-    page: usize,
-    index: usize,
-}
-
-pub struct ValueId {
-    index: usize,
-}
-
-struct Page<const N: usize> {
-    mem: Box<[Option<Value>; N]>,
-    count: usize,       // number of filled slots
-    first_empty: usize, // first empty slot
-}
-
-enum PageInsert {
-    Success(usize),
-    Failed(Value),
-}
-
-pub enum StackValue {
-    Stack(Value),
-    Heap(HeapId),
-}
+use crate::value::{Instance, Value};
 
 pub struct Heap {
     pages: Vec<Page<{ Heap::PAGE_SIZE }>>,
 }
 
 pub struct Stack {
-    stack: Vec<StackValue>,
+    values: Vec<Value>,
     blocks: Vec<usize>,
 }
 
-pub struct Memory {}
+#[derive(Debug, Error)]
+pub enum MemoryError {
+    #[error("Stack overflow (wants: {0:010x}, limit: {1:010x}")]
+    StackOverflow(usize, usize),
 
-impl<const N: usize> Page<N> {
-    pub fn new() -> Self {
-        let vec = Vec::from_iter((0..N).map(|_| Option::<Value>::None));
-        let ptr = Box::into_raw(vec.into_boxed_slice());
-        Self {
-            mem: unsafe { Box::from_raw(ptr as *mut [_; N]) },
-            count: 0,
-            first_empty: 0,
-        }
-    }
+    #[error("Stack is empty")]
+    EmptyStack,
+}
 
-    pub fn insert(&mut self, value: Value) -> PageInsert {
-        if self.count < N {
-            let i = self.first_empty;
+pub enum HeapValue {
+    Value(Value),
+    String(String),
+    Instance(Instance),
+}
 
-            self.mem[i] = Some(value);
-            self.count += 1;
+#[derive(Debug, Copy, Clone, PartialEq, PartialOrd)]
+pub struct HeapId {
+    page: usize,
+    index: usize,
+}
 
-            let first = self.mem[i..N].iter().position(|v| v.is_none());
-            self.first_empty = first.unwrap_or(N);
+#[derive(Debug, Copy, Clone)]
+pub struct ValueId {
+    index: usize,
+}
 
-            PageInsert::Success(i)
-        } else {
-            PageInsert::Failed(value)
-        }
-    }
+struct PageValue {
+    value: HeapValue,
+    mark: Mark,
+}
 
-    pub fn clear(&mut self, index: usize) {
-        self.mem[index] = None;
-        if self.first_empty > index {
-            self.first_empty = index;
-        }
-        self.count -= 1;
-    }
+struct Page<const N: usize> {
+    mem: Box<[Option<PageValue>; N]>,
+    count: usize,       // number of filled slots
+    first_empty: usize, // first empty slot
+}
 
-    pub fn get(&self, index: usize) -> Option<&Value> {
-        self.mem[index].as_ref()
-    }
-
-    pub fn replace(&mut self, index: usize, value: Value) -> Option<Value> {
-        std::mem::replace(&mut self.mem[index], Some(value))
-    }
+enum Mark {
+    Live,
+    Dead,
 }
 
 impl Heap {
@@ -90,65 +60,220 @@ impl Heap {
         Self { pages: Vec::new() }
     }
 
-    pub fn construct(&mut self, mut value: Value) -> HeapId {
+    pub fn construct(&mut self, value: HeapValue) -> HeapId {
         for (p, page) in self.pages.iter_mut().enumerate() {
-            value = match page.insert(value) {
-                PageInsert::Success(i) => return HeapId { page: p, index: i },
-                PageInsert::Failed(v) => v,
+            if page.can_insert() {
+                let i = page.insert(value);
+                return HeapId { page: p, index: i };
             }
         }
 
         self.pages.push(Page::new());
 
         let p = self.pages.len() - 1;
-        let page = &mut self.pages[p];
+        let i = (&mut self.pages[p]).insert(value);
 
-        match page.insert(value) {
-            PageInsert::Success(i) => HeapId { page: p, index: i },
-            PageInsert::Failed(_) => unreachable!(),
-        }
+        HeapId { page: p, index: i }
     }
 
-    pub fn destruct(&mut self, HeapId { page, index }: HeapId) {
+    pub fn destruct(&mut self, id: HeapId) {
+        let HeapId { page, index } = id;
         let page = &mut self.pages[page];
         page.clear(index);
     }
 
-    pub fn replace(&mut self, HeapId { page, index }: HeapId, value: Value) -> Option<Value> {
+    pub fn replace(&mut self, id: HeapId, value: HeapValue) -> Option<HeapValue> {
+        let HeapId { page, index } = id;
         let page = &mut self.pages[page];
         page.replace(index, value)
     }
 
-    pub fn get(&self, HeapId { page, index }: HeapId) -> Option<&Value> {
+    pub fn get(&self, id: HeapId) -> Option<&HeapValue> {
+        self.get_with_mark(id).map(|v| &v.value)
+    }
+
+    pub fn get_mut(&mut self, id: HeapId) -> Option<&mut HeapValue> {
+        self.get_with_mark_mut(id).map(|v| &mut v.value)
+    }
+
+    pub fn get_two(&self, one: HeapId, two: HeapId) -> Option<(&HeapValue, &HeapValue)> {
+        let (page_one, page_two) = (&self.pages[one.page], &self.pages[two.page]);
+        Some((
+            &page_one.get(one.index)?.value,
+            &page_two.get(two.index)?.value,
+        ))
+    }
+
+    pub fn get_two_mut(
+        &mut self,
+        one: HeapId,
+        two: HeapId,
+    ) -> Option<(&mut HeapValue, &mut HeapValue)> {
+        // inspired by: https://stackoverflow.com/a/74296885
+        let (start, end) = match one.page < two.page {
+            true => (one.page, two.page),
+            false => (two.page, one.page),
+        };
+        let (left, right) = self.pages.split_at_mut(start + 1);
+        let (page_one, page_two) = (&mut left[start], &mut right[end - start - 1]);
+        Some((
+            &mut page_one.get_mut(one.index)?.value,
+            &mut page_two.get_mut(two.index)?.value,
+        ))
+    }
+
+    fn get_with_mark(&self, id: HeapId) -> Option<&PageValue> {
+        let HeapId { page, index } = id;
         let page = &self.pages[page];
         page.get(index)
+    }
+
+    fn get_with_mark_mut(&mut self, id: HeapId) -> Option<&mut PageValue> {
+        let HeapId { page, index } = id;
+        let page = &mut self.pages[page];
+        page.get_mut(index)
     }
 }
 
 impl Stack {
     pub fn new() -> Self {
         Self {
-            stack: Vec::default(),
+            values: Vec::default(),
             blocks: Vec::default(),
         }
     }
 
     pub fn create_block(&mut self) {
-        self.blocks.push(self.stack.len())
+        self.blocks.push(self.values.len())
     }
 
     pub fn drop_block(&mut self) {
         let block = self.blocks.pop().expect("excessive drop");
-        self.stack.truncate(block);
+        self.values.truncate(block);
     }
 
-    pub fn push(&mut self, value: StackValue) -> ValueId {
-        let i = self.stack.len();
-        self.stack.push(value);
+    pub fn push(&mut self, value: Value) -> ValueId {
+        let i = self.values.len();
+        self.values.push(value);
         ValueId { index: i }
     }
 
-    pub fn pop(&mut self) -> Option<StackValue> {
-        self.stack.pop()
+    pub fn dup(&mut self, index: usize) -> Result<(), MemoryError> {
+        let value = self.peek(index)?;
+        self.values.push(value.clone());
+        Ok(())
+    }
+
+    pub fn pop(&mut self) -> Result<Value, MemoryError> {
+        self.values.pop().ok_or(MemoryError::EmptyStack)
+    }
+
+    pub fn top(&self) -> Result<&Value, MemoryError> {
+        self.values.last().ok_or(MemoryError::EmptyStack)
+    }
+
+    pub fn top_mut(&mut self) -> Result<&mut Value, MemoryError> {
+        self.values.last_mut().ok_or(MemoryError::EmptyStack)
+    }
+
+    pub fn peek(&self, index: usize) -> Result<&Value, MemoryError> {
+        self.values
+            .get(index)
+            .ok_or(MemoryError::StackOverflow(index, self.values.len()))
+    }
+
+    pub fn peek_mut(&mut self, index: usize) -> Result<&mut Value, MemoryError> {
+        let len = self.values.len();
+        self.values
+            .get_mut(index)
+            .ok_or(MemoryError::StackOverflow(index, len))
+    }
+}
+
+impl<const N: usize> Page<N> {
+    pub fn new() -> Self {
+        let vec = Vec::from_iter((0..N).map(|_| Option::<PageValue>::None));
+        let ptr = Box::into_raw(vec.into_boxed_slice());
+        Self {
+            mem: unsafe { Box::from_raw(ptr as *mut [_; N]) },
+            count: 0,
+            first_empty: 0,
+        }
+    }
+
+    fn can_insert(&self) -> bool {
+        self.count < N
+    }
+
+    fn insert(&mut self, value: HeapValue) -> usize {
+        let i = self.first_empty;
+
+        self.mem[i] = Some(PageValue {
+            value,
+            mark: Mark::Dead,
+        });
+        self.count += 1;
+
+        let first = self.mem[i..N].iter().position(|v| v.is_none());
+        self.first_empty = first.unwrap_or(N);
+
+        i
+    }
+
+    fn clear(&mut self, index: usize) {
+        self.mem[index] = None;
+        if self.first_empty > index {
+            self.first_empty = index;
+        }
+        self.count -= 1;
+    }
+
+    fn get(&self, index: usize) -> Option<&PageValue> {
+        self.mem[index].as_ref()
+    }
+
+    fn get_mut(&mut self, index: usize) -> Option<&mut PageValue> {
+        self.mem[index].as_mut()
+    }
+
+    fn replace(&mut self, index: usize, value: HeapValue) -> Option<HeapValue> {
+        std::mem::replace(
+            &mut self.mem[index],
+            Some(PageValue {
+                value,
+                mark: Mark::Dead,
+            }),
+        )
+        .map(|v| v.value)
+    }
+}
+
+impl HeapValue {
+    pub fn as_string_mut(&mut self) -> &mut String {
+        match self {
+            HeapValue::String(s) => s,
+            _ => panic!("internal heap value is not a string!"),
+        }
+    }
+
+    pub fn as_string(&self) -> &String {
+        match self {
+            HeapValue::String(s) => s,
+            _ => panic!("internal heap value is not a string!"),
+        }
+    }
+}
+
+pub fn gc(stack: &Stack, heap: &mut Heap) {
+    for v in stack.values.iter() {
+        match v {
+            Value::Nil => todo!(),
+            Value::Bool(_) => todo!(),
+            Value::Number(_) => todo!(),
+            Value::String(heap_id) => todo!(),
+            Value::Class(class_id) => todo!(),
+            Value::Instance(heap_id) => todo!(),
+            Value::Function(func_id, heap_id) => todo!(),
+        }
     }
 }
--- loxii/src/value.rs
+++ loxii/src/value.rs
@@ -1,20 +1,23 @@
-use std::rc::Rc;
+use std::fmt::Display;
 
 use rustc_hash::FxHashMap;
+use thiserror::Error;
 
-use crate::bytecode::Address;
-use crate::memory::HeapId;
+use crate::memory::{Heap, HeapId};
+use crate::metadata::{ClassId, FuncId};
 
+#[derive(Debug, Clone)]
 pub enum Value {
     Nil,
     Bool(bool),
     Number(f64),
-    Class(Rc<Class>),
-    String(Rc<String>),
-    Function(Rc<Function>),
-    Instance(Rc<Instance>),
+    String(HeapId),
+    Class(ClassId),
+    Instance(HeapId),
+    Function(FuncId, Option<HeapId>), // HeapId must be instance
 }
 
+#[derive(Debug)]
 pub enum Constant<'a> {
     Nil,
     Bool(bool),
@@ -22,18 +25,168 @@ pub enum Constant<'a> {
     String(&'a str),
 }
 
-pub struct Function {
-    captures: Vec<HeapId>,
-    address: Address,
-}
-
-pub struct Class {
-    constructor: Option<Function>,
-    methods: FxHashMap<String, Function>,
-}
-
+#[derive(Debug)]
 pub struct Instance {
     id: usize,
-    class: *const Class,
+    class: ClassId,
     fields: FxHashMap<String, Value>,
+}
+
+#[derive(Debug, Error)]
+pub enum ValueError {
+    #[error("Invalid unary operation '{0}' on '{1}'")]
+    Unary(&'static str, &'static str),
+
+    #[error("Invalid binary operation '{0}' between '{1}' and '{2}'")]
+    Binary(&'static str, &'static str, &'static str),
+}
+
+pub struct DisplayedValue<'a, 'b> {
+    value: &'a Value,
+    heap: &'b Heap,
+}
+
+impl Value {
+    pub fn name(&self) -> &'static str {
+        match self {
+            Value::Nil => "<nil>",
+            Value::Bool(_) => "<bool>",
+            Value::Number(_) => "<number>",
+            Value::String(_) => "<string>",
+            Value::Class(_) => "<class>",
+            Value::Instance(_) => "<instance>",
+            Value::Function(_, _) => "<function>",
+        }
+    }
+
+    pub fn display<'a, 'b>(&'a self, heap: &'b Heap) -> DisplayedValue<'a, 'b> {
+        DisplayedValue { value: self, heap }
+    }
+
+    pub fn truthy(&self) -> bool {
+        match self {
+            Value::Nil => false,
+            Value::Bool(b) => *b,
+            _ => true,
+        }
+    }
+
+    pub fn not(&mut self) {
+        *self = Value::Bool(!self.truthy());
+    }
+
+    pub fn negate(&mut self) -> Result<(), ValueError> {
+        if let Value::Number(n) = self {
+            *n = -*n;
+            return Ok(());
+        };
+        Err(ValueError::Unary("-", self.name()))
+    }
+
+    pub fn add(self, other: Self, heap: &mut Heap) -> Result<Value, ValueError> {
+        match (self, other) {
+            (Value::Number(num1), Value::Number(num2)) => Ok(Value::Number(num1 + num2)),
+            (Value::String(str1), Value::String(str2)) => {
+                let (strr1, strr2) = heap.get_two_mut(str1, str2).unwrap();
+                let strr1 = strr1.as_string_mut();
+                let strr2 = strr2.as_string();
+                strr1.push_str(strr2);
+                Ok(Value::String(str1))
+            }
+            (lhs, rhs) => Err(ValueError::Binary("+", lhs.name(), rhs.name())),
+        }
+    }
+
+    pub fn sub(self, other: Self) -> Result<Value, ValueError> {
+        match (self, other) {
+            (Value::Number(num1), Value::Number(num2)) => Ok(Value::Number(num1 - num2)),
+            (lhs, rhs) => Err(ValueError::Binary("-", lhs.name(), rhs.name())),
+        }
+    }
+
+    pub fn mul(self, other: Self) -> Result<Value, ValueError> {
+        match (self, other) {
+            (Value::Number(num1), Value::Number(num2)) => Ok(Value::Number(num1 * num2)),
+            (lhs, rhs) => Err(ValueError::Binary("*", lhs.name(), rhs.name())),
+        }
+    }
+
+    pub fn div(self, other: Self) -> Result<Value, ValueError> {
+        match (self, other) {
+            (Value::Number(num1), Value::Number(num2)) => Ok(Value::Number(num1 / num2)),
+            (lhs, rhs) => Err(ValueError::Binary("/", lhs.name(), rhs.name())),
+        }
+    }
+
+    pub fn eq(&self, other: &Self, heap: &Heap) -> Value {
+        Value::Bool(self.is_equal(other, heap))
+    }
+
+    pub fn neq(&self, other: &Self, heap: &Heap) -> Value {
+        Value::Bool(!self.is_equal(other, heap))
+    }
+
+    pub fn gt(&self, other: &Self) -> Result<Value, ValueError> {
+        match (self, other) {
+            (Value::Number(num1), Value::Number(num2)) => Ok(Value::Bool(*num1 > *num2)),
+            (lhs, rhs) => Err(ValueError::Binary(">", lhs.name(), rhs.name())),
+        }
+    }
+
+    pub fn ge(&self, other: &Self) -> Result<Value, ValueError> {
+        match (self, other) {
+            (Value::Number(num1), Value::Number(num2)) => Ok(Value::Bool(*num1 >= *num2)),
+            (lhs, rhs) => Err(ValueError::Binary(">=", lhs.name(), rhs.name())),
+        }
+    }
+
+    pub fn lt(&self, other: &Self) -> Result<Value, ValueError> {
+        match (self, other) {
+            (Value::Number(num1), Value::Number(num2)) => Ok(Value::Bool(*num1 < *num2)),
+            (lhs, rhs) => Err(ValueError::Binary("<", lhs.name(), rhs.name())),
+        }
+    }
+
+    pub fn le(&self, other: &Self) -> Result<Value, ValueError> {
+        match (self, other) {
+            (Value::Number(num1), Value::Number(num2)) => Ok(Value::Bool(*num1 <= *num2)),
+            (lhs, rhs) => Err(ValueError::Binary("<=", lhs.name(), rhs.name())),
+        }
+    }
+
+    fn is_equal(&self, other: &Self, heap: &Heap) -> bool {
+        match (self, other) {
+            (Value::Nil, Value::Nil) => true,
+            (Value::Bool(b1), Value::Bool(b2)) => b1 == b2,
+            (Value::Number(num1), Value::Number(num2)) => num1 == num2,
+            (Value::Class(cls1), Value::Class(cls2)) => cls1 == cls2,
+            (Value::Instance(ins1), Value::Instance(ins2)) => ins1 == ins2,
+            (Value::Function(func1, ins1), Value::Function(func2, ins2)) => {
+                func1 == func2 && ins1 == ins2
+            }
+            (Value::String(str1), Value::String(str2)) => {
+                let (str1, str2) = heap.get_two(*str1, *str2).unwrap();
+                str1.as_string() == str2.as_string()
+            }
+            _ => false,
+        }
+    }
+}
+
+impl Display for DisplayedValue<'_, '_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let Self { value, heap } = self;
+        match value {
+            Value::Nil => write!(f, "nil"),
+            Value::Bool(b) => write!(f, "{b}"),
+            Value::Number(num) => write!(f, "{num}"),
+            Value::String(id) => {
+                let str = heap.get(*id).ok_or(std::fmt::Error)?.as_string();
+                write!(f, "{str}")
+            }
+            Value::Class(id) => todo!(),
+            Value::Instance(id) => todo!(),
+            Value::Function(id, inst) => todo!(),
+        }
+    }
 }
